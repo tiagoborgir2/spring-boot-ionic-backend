4cc8f3795d43f2310adc09231ffea1866ca13e7f
Tiago Costa <tiago.alcosta@gmail.com>
2021-10-21 15:45:03 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
@@ -1,10 +1,29 @@
 package com.nelioalves.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIN(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable pageRequest);
+	
+	/*
+	 * Isso faz o mesmo q a linha 20 e 21. Automaticamente o Spring gera a consulta.
+	 * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+	 * 
+	 * Page<Produto> findDistinctByNomeContainingAndCategoriasIN( String nome, List<Categoria> categorias, Pageable pageRequest);
+	 */
 }
